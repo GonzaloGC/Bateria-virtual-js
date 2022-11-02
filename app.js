4bdf1b841127a64cf12a1b7032fe4d9a343260e8
Gonzalo Guerrero <elias.gonzaloguerrero@gmail.com>
2022-11-02 00:34:07 -0300

    added alert

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+window.alert("Para tocar la bateria, PRESIONA la letra en tu teclado")
+
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
-  }
+}
 
-  function playSound(e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
-
+    
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
-  }
+}
 
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
